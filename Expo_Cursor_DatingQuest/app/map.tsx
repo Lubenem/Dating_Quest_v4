4ddcc4e3d8c5b724c8c5f549559872c284192a63
Lubenem <1720733-Lubenem@users.noreply.replit.com>
2025-10-07 12:17:18 +0000

    Restored to '95ea1cab3e5b8751dd88644fe83c17d3fc9172e3'
    Replit-Restored-To: 95ea1cab3e5b8751dd88644fe83c17d3fc9172e3

--- Expo_Cursor_DatingQuest/app/map.tsx
+++ Expo_Cursor_DatingQuest/app/map.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { MapContent } from '../components/Map/MapContent';
+import { LeafletMapWebView } from '../components/Map/LeafletMapWebView';
 
 export default function MapScreen() {
-  return <MapContent />;
+  return <LeafletMapWebView />;
 }
